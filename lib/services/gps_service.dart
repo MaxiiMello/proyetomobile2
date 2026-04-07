27e0224bdabb03faab47514986f68fa98edaad2c
Felipe de Avila Segui <felipedeavila89@gmail.com>
2026-04-07 20:16:59 -0300

    Agregado de extension geolocator + ajustes de dependencias Ios y Android

--- lib/services/gps_service.dart
+++ lib/services/gps_service.dart
@@ -2,18 +2,15 @@ import 'package:geolocator/geolocator.dart';
 
 class GpsService {
   
-  // Función principal para obtener la posición actual
   Future<Position> determinarPosicion() async {
     bool servicioHabilitado;
     LocationPermission permiso;
 
-    // 1. Verificar si el GPS del celular está encendido
     servicioHabilitado = await Geolocator.isLocationServiceEnabled();
     if (!servicioHabilitado) {
       return Future.error('El servicio de localización está desactivado.');
     }
 
-    // 2. Manejar los permisos
     permiso = await Geolocator.checkPermission();
     if (permiso == LocationPermission.denied) {
       permiso = await Geolocator.requestPermission();
@@ -26,7 +23,6 @@ class GpsService {
       return Future.error('Los permisos están denegados permanentemente.');
     } 
 
-    // 3. Si todo está bien, obtener la ubicación con alta precisión
     return await Geolocator.getCurrentPosition(
       desiredAccuracy: LocationAccuracy.high
     );
